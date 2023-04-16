6b8aea810224523e1e34be5a61df6d2c8d754a5e
TheDarkLord100 <miracleyt404@gmail.com>
2023-04-16 14:11:35 +0530

    Added buttonpanels for stack and queue

--- project.java
+++ project.java
@@ -1,4 +1,4 @@
-import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -13,7 +13,7 @@ class Project {
         Project obj = new Project();
 
         JFrame frame = new JFrame("Vijayant Singh");
-        frame.setSize(400, 200);
+        frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new GridBagLayout());
@@ -42,13 +42,30 @@ class Project {
         group.add(stackButton);
 
         LinkedList<Integer> list = obj.getList();
-
         JLabel listLabel = new JLabel(list.toString());
         c.gridx = 0;
         c.gridy = 2;
         c.gridwidth = 2;
         c.insets = new Insets(20, 0, 0, 0);
         panel.add(listLabel, c);
+
+        JPanel queueButtonPanel = new JPanel(new FlowLayout());
+        JButton addButton = new JButton("Add");
+        JButton deleteButton = new JButton("Delete");
+        queueButtonPanel.add(addButton);
+        queueButtonPanel.add(deleteButton);
+
+        c.gridy = 3;
+        panel.add(queueButtonPanel, c);
+
+        JPanel stackButtonPanel = new JPanel(new FlowLayout());
+        JButton pushButton = new JButton("Push");
+        JButton popButton = new JButton("Pop");
+        stackButtonPanel.add(pushButton);
+        stackButtonPanel.add(popButton);
+
+        c.gridy = 4;
+        panel.add(stackButtonPanel, c);
 
         frame.add(panel);
         frame.setVisible(true);
