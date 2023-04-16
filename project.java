3053eb17a7bab0855a37c59ad1955a2e81e94f8c
TheDarkLord100 <miracleyt404@gmail.com>
2023-04-16 13:46:35 +0530

    Changed alignment of heading and buttons

--- project.java
+++ project.java
@@ -1,27 +1,36 @@
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.*;
 
 class Project {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Vijayant Singh");
-        frame.setSize(300, 150);
+        frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.VERTICAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 8, 20, 8);
+
+
         JLabel buttonHeading = new JLabel("Data Structure");
-        panel.add(buttonHeading);
-        buttonHeading.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(buttonHeading, c);
 
         JRadioButton stackButton = new JRadioButton("Stack");
         JRadioButton queueButton = new JRadioButton("Queue");
-        stackButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        queueButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(0, 0, 0, 0);
+        panel.add(stackButton, c);
 
-        panel.add(stackButton);
-        panel.add(queueButton);
+        c.gridx = 1;
+        panel.add(queueButton, c);
 
         ButtonGroup group = new ButtonGroup();
         group.add(queueButton);
